eea554c4b46175b8a0d2118df5ea82a0968be5e2
Sue Holder <susanpholder@gmail.com>
2019-12-11 20:02:37 +0000

    moved graph to charts.js transposed data rows

--- static/js/charts.js
+++ static/js/charts.js
@@ -1,8 +1,40 @@
 queue()
-    .defer(d3.tsv, "data/vote-share.tsv")
-    .await(makeCharts);
+      .defer(d3.json, "data/votes-M.json")
+      .await(makeGraphs);
 
-function makeCharts(error, votesData) {
+    function makeGraphs(error, votesData) {
+      if (error) throw error;
+
+      var ndx = crossfilter(votesData);
+
+      var year_dim = ndx.dimension(dc.pluck("YEAR"));
+      var total_votes_per_year = year_dim.group().reduceSum(dc.pluck("CON2"));
+
+      dc.barChart("#CON2-votes-per-year")
+        .width(800)
+        .height(350)
+        .margins({
+          top: 10,
+          right: 50,
+          bottom: 40,
+          left: 20
+        })
+        .dimension(year_dim)
+        .group(total_votes_per_year)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .xAxisLabel("Votes (Millions) per Year")
+
+        .yAxis().ticks(10);
+
+      dc.renderAll();
+    }
+
+    function compositeChart(error, shareData) {
+      if (error) throw error;
+
+      var ndx = crossfilter(shareData);
 
 
-}
+    }
