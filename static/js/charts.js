ad17a4410c0348db66e17f7dc3581e99bcce37d1
Sue Holder <susanpholder@gmail.com>
2019-12-13 14:17:32 +0000

    update chart.js with stackedChart

--- static/js/charts.js
+++ static/js/charts.js
@@ -1,65 +1,84 @@
 queue()
-    .defer(d3.json, "data/votes-M.json")
-    .await(makeGraphs);
+  .defer(d3.tsv, "data/votes-M.tsv")
+  .await(makeGraphs);
 
 function makeGraphs(error, votesData) {
-    if (error) throw error;
+  if (error) throw error;
 
-    var ndx = crossfilter(votesData);
+  let ndx = crossfilter(votesData);
 
-    var year_dim = ndx.dimension(dc.pluck("YEAR"));
-    var total_votes_per_year = year_dim.group().reduceSum(dc.pluck("CON2"));
-
-    dc.barChart("#CON2-votes-per-year")
-        .width(800)
-        .height(350)
-        .margins({
-            top: 10,
-            right: 50,
-            bottom: 40,
-            left: 20
-        })
-        .dimension(year_dim)
-        .group(total_votes_per_year)
-        .transitionDuration(500)
-        .x(d3.scale.ordinal())
-        .xUnits(dc.units.ordinal)
-        .xAxisLabel("Votes (Millions) per Year")
-
-        .yAxis().ticks(10);
-
-    dc.renderAll();
+  show_votes_comparison(ndx);
 }
 
-queue()
-    .defer(d3.tsv, "data/votes-M-transp.tsv")
-    .await(compareVotes);
+function show_votes_comparison(ndx) {
+  let year_dim = ndx.dimension(dc.pluck("Year"));
 
-function compareVotes(error, votesData) {
-    if (error) throw error;
+  let votes_per_year_CON2 = year_dim.group().reduceSum(function(d) {
+    if (d.Party === "CON2") {
+      return d.Votes;
+    } else {
+      return 0;
+    }
+  });
 
-    var ndx = crossfilter(votesData);
+  let votes_per_year_LAB = year_dim.group().reduceSum(function(d) {
+    if (d.Party === "LAB") {
+      return d.Votes;
+    } else {
+      return 0;
+    }
+  });
 
-    var party_dim = ndx.dimension(dc.pluck("Party"));
-    var total_votes_per_year = party_dim.group().reduceSum(dc.pluck("1917"));
+  let votes_per_year_LD3 = year_dim.group().reduceSum(function(d) {
+    if (d.Party === "LD3") {
+      return d.Votes;
+    } else {
+      return 0;
+    }
+  });
 
-    dc.barChart("#compare-votes-per-year")
-        .width(800)
-        .height(350)
-        .margins({
-            top: 10,
-            right: 50,
-            bottom: 40,
-            left: 20
-        })
-        .dimension(party_dim)
-        .group(total_votes_per_year)
-        .transitionDuration(500)
-        .x(d3.scale.ordinal())
-        .xUnits(dc.units.ordinal)
-        .xAxisLabel("Votes (Millions) per Year")
+  let votes_per_year_scot = year_dim.group().reduceSum(function(d) {
+    if (d.Party === "PC/SNP") {
+      return d.Votes;
+    } else {
+      return 0;
+    }
+  });
 
-        .yAxis().ticks(10);
+  let votes_per_year_others = year_dim.group().reduceSum(function(d) {
+    if (d.Party === "Other") {
+      return d.Votes;
+    } else {
+      return 0;
+    }
+  });
 
-    dc.renderAll();
+  let stackedChart = dc.barChart("#compare-votes-per-year");
+
+  stackedChart
+    .width(1000)
+    .height(1000)
+    .dimension(year_dim)
+    .group(votes_per_year_CON2, "Conservative")
+    .stack(votes_per_year_LAB, "Labour")
+    .stack(votes_per_year_LD3, "Lib Dem")
+    .stack(votes_per_year_scot, "PC/SNP")
+    .stack(votes_per_year_others, "All Other")
+    .x(d3.scale.ordinal())
+    .xUnits(dc.units.ordinal)
+    .xAxisLabel("Votes in Millions per party per year")
+    .margins({top: 10, right: 50, bottom: 50, left: 50})
+    .legend(
+      dc
+        .legend()
+        .x(720)
+        .y(0)
+        .itemHeight(15)
+        .gap(5)
+    );
+
+  
+
+  dc.renderAll();
+
 }
