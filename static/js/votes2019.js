c594fba3b6eb628a1ccf5d6ea4e9ed5bacf97beb
Sue Holder <susanpholder@gmail.com>
2020-01-07 15:58:20 +0000

    remove unsued data files

--- static/js/votes2019.js
+++ static/js/votes2019.js
@@ -7,14 +7,23 @@ function makeGraphs(error, latestData) {
 
     let ndx = crossfilter(latestData);
 
+    // filter null values
+    let filteredData = latestData.filter(function (d) {
+        if (d.seats >= 1) {
+            return d.value;
+        }
+    })
     // parse data
     latestData.forEach(function (d) {
         d.party = d.party
         d.votes = +d.votes;
         d.seats = +d.seats;
+        d.votesShare = +d.votesShare
     });
 
+
     show_votes(ndx)
+    show_seats(ndx)
 
     dc.renderAll();
 
@@ -31,5 +40,24 @@ function show_votes(ndx) {
         .transitionDuration(1000)
         .dimension(party_dim)
         .group(votes_per_party)
+
+}
+
+function show_seats(ndx) {
+
+
+    let party_dim = ndx.dimension(dc.pluck("party"));
+    let seats_per_party = party_dim.group().reduceSum(dc.pluck("seats"));
+
+
+    dc.pieChart("#votes-2019")
+        .height(340)
+        .radius(150)
+        .useViewBoxResizing(true)
+        .transitionDuration(1000)
+        .dimension(party_dim)
+        .group(seats_per_party)
+
+
 
 }
