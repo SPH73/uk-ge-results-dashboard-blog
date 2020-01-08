3ebe2aa11447213ac864a3ac894bc9a6cbe8909d
Sue Holder <susanpholder@gmail.com>
2020-01-08 10:30:29 +0000

    bubble chart on change.js and change.html test

--- static/js/change.js
+++ static/js/change.js
@@ -10,60 +10,17 @@ function makeGraphs(error, latestData) {
     // parse data
     latestData.forEach(function (d) {
         d.party = d.party
-        d.votes = Number(+d.votes);
+        d.votes = +d.votes;
         d.seats = +d.seats;
         d.seatChange = +d.seatChange
         d.voteShare = +d.voteShare
         d.voteShareChange = +d.voteShareChange
     });
 
-    show_party_selector(ndx)
-    show_votes(ndx)
-    show_seats(ndx)
+
     show_correlation(ndx)
 
     dc.renderAll();
-
-}
-
-function show_party_selector(ndx) {
-
-    let dim = ndx.dimension(dc.pluck('party'));
-    let group = dim.group();
-
-    dc.selectMenu("#select-menu")
-        .dimension(dim)
-        .group(group);
-}
-
-function show_votes(ndx) {
-
-    let party_dim = ndx.dimension(dc.pluck("party"));
-    let votes_per_party = party_dim.group().reduceSum(dc.pluck("votes"));
-
-    dc.pieChart("#votes-2019")
-        .height(340)
-        .radius(150)
-        .useViewBoxResizing(true)
-        .transitionDuration(1000)
-        .dimension(party_dim)
-        .group(votes_per_party)
-
-}
-
-function show_seats(ndx) {
-
-    let party_dim = ndx.dimension(dc.pluck("party"));
-    let seats_per_party = party_dim.group().reduceSum(dc.pluck("seats"));
-
-
-    dc.pieChart("#seats-2019")
-        .height(340)
-        .radius(150)
-        .useViewBoxResizing(true)
-        .transitionDuration(1000)
-        .dimension(party_dim)
-        .group(seats_per_party)
 
 }
 
@@ -101,9 +58,8 @@ function show_correlation(ndx) {
         .colors(d3.scale.category10())
 
 
-}
 
 
-function resetChart() {
-    window.location.reload();
+
+
 }
